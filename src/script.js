3503ef7ba1aba23c57e5761c4b171dd57a6f9723
kai <edilson.gs1602@gmail.com>
2024-02-29 13:50:02 -0300

    finalized web dark mode and file organization

---
+++ src/script.js
@@ -0,0 +1,103 @@
+//MENU NAVEGATION
+const navLinks = document.querySelectorAll(".nav-link, .nav-link-footer");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const targetId = link.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+//DARK-MODE
+function toggleDarkMode() {
+  const portal = document.getElementById("portal");
+  const profileImage = document.getElementById("profileImage");
+  const darkModeButton = document.getElementById("darkModeButton");
+  const backgroundComment = document.getElementById("background-comment");
+
+  document.body.classList.toggle("dark-mode");
+
+  const isDarkMode = document.body.classList.contains("dark-mode");
+
+  updateTheme(
+    isDarkMode,
+    portal,
+    profileImage,
+    darkModeButton,
+    backgroundComment
+  );
+
+  toggleSocialMediaImages();
+}
+
+function toggleSocialMediaImages() {
+  const images = document.querySelectorAll(".icons-media img");
+  images.forEach((img) => {
+    let src = img.getAttribute("src");
+    if (src.includes("/light-mode/")) {
+      src = src.replace("/light-mode/", "/dark-mode/");
+    } else if (src.includes("/dark-mode/")) {
+      src = src.replace("/dark-mode/", "/light-mode/");
+    }
+    img.setAttribute("src", src);
+  });
+}
+
+function updateTheme(
+  isDarkMode,
+  portal,
+  profileImage,
+  darkModeButton,
+  backgroundComment
+) {
+  if (isDarkMode) {
+    backgroundComment.src = "src/assets/img/background-comment-dark-mode.png";
+    portal.src = "src/assets/img/Portal-white.svg";
+    profileImage.src = "src/assets/img/Logo-white.png";
+    darkModeButton.innerText = "Contact Me";
+    removeImagesOnDarkMode();
+  } else {
+    backgroundComment.src = "src/assets/img/background-comment.png";
+    portal.src = "src/assets/img/Portal.png";
+    profileImage.src = "src/assets/img/Logo.png";
+    darkModeButton.innerText = "Button";
+  }
+
+  const dawnImages = document.querySelectorAll(".dawn");
+  dawnImages.forEach((img) => {
+    if (isDarkMode) {
+      img.src = "src/assets/img/Dawn-dark-mode.svg";
+    } else {
+      img.src = "src/assets/img/Dawn.png";
+    }
+  });
+}
+
+function removeImagesOnDarkMode() {
+  const dawnWhiteImage = document.getElementById("dawn-white");
+  const warmImage = document.getElementById("warm");
+  dawnWhiteImage.style.display = "none";
+  warmImage.style.display = "none";
+}
+
+(function () {
+  document
+    .getElementById("toggleContainer")
+    .addEventListener("click", function () {
+      toggleDarkMode();
+    });
+
+  document
+    .getElementById("darkModeButton")
+    .addEventListener("click", function () {
+      if (!document.body.classList.contains("dark-mode")) {
+        toggleDarkMode();
+      }
+    });
+})();
