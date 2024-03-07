633fdccb60343bdae757689fb6c0100ef7bd1320
kai <edilson.gs1602@gmail.com>
2024-03-07 12:16:30 -0300

    mobile responsive and max-width

--- src/script.js
+++ src/script.js
@@ -111,3 +111,12 @@ function removeImagesOnDarkMode() {
       }
     });
 })();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  const nav = document.querySelector(".nav");
+
+  menuIcon.addEventListener("click", function () {
+    nav.classList.toggle("active");
+  });
+});
